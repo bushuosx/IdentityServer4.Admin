f09d21cf0c572b4fe002fbbde76aa10f9ad91702
jiangjun <bushuosx@live.com>
2018-10-21 23:18:44 +0800

    YYServer path

--- src/Skoruba.IdentityServer4.Admin/Configuration/Users.cs
+++ src/Skoruba.IdentityServer4.Admin/Configuration/Users.cs
@@ -3,7 +3,7 @@
     public class Users
     {
         public const string AdminUserName = "admin";
-        public const string AdminPassword = "Pa$$word123";
+        public const string AdminPassword = "Pa$$word123000";
         public const string AdminEmail = "admin@example.com";
     }
 }
